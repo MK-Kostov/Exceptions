1f1e5aca3562371762d327eea0a41ebe9b2802ba
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-10 11:55:45 -0500

    Try – catch-finally

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a number");
+string input = Console.ReadLine();
+try
+{
+	var number = ParseStringToInt(input);
+	Console.WriteLine("String successfully parsed, the result is " + number);
+
+}
+catch (Exception ex)
+{
+	Console.WriteLine("An exception was thrown." + ex.Message);
+}
+finally
+{
+	Console.WriteLine("Finally block is being executed.");
+}
+
+Console.ReadKey();
+
+int ParseStringToInt(string input)
+{
+	return int.Parse(input);
+}
